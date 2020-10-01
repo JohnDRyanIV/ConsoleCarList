9e9de896c4448d8af2f759f89972c8f2c98775c5
JohnDRyanIV <43046528+JohnDRyanIV@users.noreply.github.com>
2020-10-01 03:27:33 -0700

    begun implementing tomcat functionality

---
+++ ConsoleCarList/src/controller/AddItemServlet.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.ListItem;
+
+/**
+ * Servlet implementation class AddItemServlet
+ */
+@WebServlet("/addItemServlet")
+public class AddItemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddItemServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// requesting make, model, and year from index
+		String make = request.getParameter("make");
+		String model = request.getParameter("model");
+		String year = request.getParameter("year");
+		
+		// creating new listItem instance and using listItemHelper to add that instance to the database
+		ListItem li = new ListItem(make, model, year);
+		ListItemHelper dao = new ListItemHelper();
+		dao.insertItem(li);
+		
+		getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+	}
+
+}
